af1ccef4bed03449db5a1733e38a83f5fc211f49
Kathleen Yoannon <k.yoannon@gmail.com>
2020-08-17 17:31:00 -0700

    fixed app issues and lib callbacks

--- app.js
+++ app.js
@@ -3,19 +3,10 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
-<<<<<<< HEAD
 const RESULT_DIR = path.resolve(__dirname, "result");
 const resultPath = path.join(RESULT_DIR, "result.html");
 
 const render = require("./lib/htmlRenderer");
-=======
-const path = require("path");
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./lib/htmlRenderer");
-
-//General questions
->>>>>>> 6adfc120f06a6b54d6a621daf7a688ee7d955c60
 const questions = [
     {
         type: "input",
@@ -100,10 +91,5 @@ async function ask() {
         });
     }
 }
-<<<<<<< HEAD
  
 ask();
-=======
-// Calls the ask() function and initiates inquire.prompt 
-ask();
->>>>>>> 6adfc120f06a6b54d6a621daf7a688ee7d955c60
